ad59d690dcf4acd7f2583547daca93f1ab85cc73
benzjamin0 <36101433+benjaminlim00@users.noreply.github.com>
2018-11-14 20:48:09 +0800

    Initial commit - Emulator works but device unable to send picture to database.

---
+++ app/src/main/java/com/example/benjamin/sutdregistration/CameraActivity.java
@@ -0,0 +1,201 @@
+package com.example.benjamin.sutdregistration;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import java.io.ByteArrayOutputStream;
+
+
+public class CameraActivity extends AppCompatActivity {
+
+    private Button buttonImage;
+    private ImageView imageView;
+    ContentValues values;
+    Uri imageUri;
+    String imageurl;
+    private String studentID;
+
+    private static final int REQUEST_ID_READ_WRITE_PERMISSION = 99;
+    private static final int REQUEST_ID_IMAGE_CAPTURE = 100;
+
+    //creating reference to firebase storage
+    //THIS IS NEEDED!!!
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_page);
+
+        //initialize firebase, this is not needed
+//        storage = FirebaseStorage.getInstance();
+//        storageReference = storage.getReference();
+
+
+        //To fix permission issues
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            int readPermission = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+            int writePermission = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int internetPermission = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_NETWORK_STATE);
+            int networkPermission = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_NETWORK_STATE);
+
+
+            if (writePermission != PackageManager.PERMISSION_GRANTED ||
+                    readPermission != PackageManager.PERMISSION_GRANTED ||
+                    internetPermission != PackageManager.PERMISSION_GRANTED ||
+                    networkPermission != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                this.requestPermissions(
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.ACCESS_NETWORK_STATE,
+                                Manifest.permission.INTERNET},
+                        REQUEST_ID_READ_WRITE_PERMISSION
+                );
+            }
+        }
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            studentID = extras.getString("STUDENT_ID");
+        }
+
+        this.buttonImage = this.findViewById(R.id.button_image);
+        this.imageView = this.findViewById(R.id.imageView);
+
+        this.buttonImage.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+    }
+
+    private void captureImage() {
+        values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        // Create an implicit intent, for image capture.
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        // Start camera and wait for the results.
+        startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
+
+
+
+    }
+
+
+    // When you have the request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        switch (requestCode) {
+            case REQUEST_ID_READ_WRITE_PERMISSION: {
+
+                // Note: If request is cancelled, the result arrays are empty.
+                // Permissions granted (read/write).
+                if (grantResults.length > 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[3] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+
+                }
+                // Cancelled or denied.
+                else {
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    // When results returned
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ID_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    Bitmap pic = MediaStore.Images.Media.getBitmap(
+                            getContentResolver(), imageUri);
+                    this.imageView.setImageBitmap(pic);
+                    imageurl = getRealPathFromURI(imageUri);
+
+                    //encoding the image to base64 string
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    pic.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                    String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
+                    //TODO, send pic to firebase - Done
+                    //we send the string over to firebase
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("Photo");
+                    myRef.setValue(imageEncoded);
+
+                    //TODO send studentID to firebase - Done
+                    DatabaseReference myRef2 = database.getReference("Student Id");
+                    myRef2.setValue(studentID);
+
+                    //TODO send array over, does not overwrite old data.
+
+
+                    Toast.makeText(CameraActivity.this, "Uploading finished :)", Toast.LENGTH_LONG).show();
+
+//                        final ProgressDialog progressDialog = new ProgressDialog(this);
+//                        progressDialog.setTitle("Uploading...");
+//                        progressDialog.show();
+
+                } catch (Exception e) {
+                    Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+                }
+
+            }   else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Action canceled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Action Failed", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+}
---
+++ app/src/main/java/com/example/benjamin/sutdregistration/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.benjamin.sutdregistration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button register;
+    EditText studentid;
+    String studentidContent;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MainActivity.this, CameraActivity.class);
+        takePictureIntent.putExtra("STUDENT_ID", studentidContent);
+        startActivity(takePictureIntent);
+    }
+
+    public boolean isStringInt(String s)
+    {
+        try
+        {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException ex)
+        {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        studentid = findViewById(R.id.idfield);
+
+        //TODO make it so that i can press enter button to hide keyboard - Done
+        studentid.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            //added single line function in xml
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    Log.e("Ben","Enter key pressed");
+
+                }
+                return false;
+            }
+        });
+
+
+
+
+        register = findViewById(R.id.registerbutton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                studentidContent = studentid.getText().toString();
+                if (studentidContent.length() == 0  || !isStringInt(studentidContent)) {   //take note that getString == "" or null, does not work
+                    //TODO make it so that it only accept digits - Done
+                    Toast.makeText(MainActivity.this, "Please key in your student id", Toast.LENGTH_LONG).show();
+                    Log.i("Ben", "failure");
+                    Log.i("Ben", studentidContent);
+
+                } else {
+                    //TODO testing only
+                    // Write a message to the database
+//                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+//                    DatabaseReference myRef = database.getReference("message");
+//
+//                    myRef.setValue("Please show up");
+                    //TODO END
+                    dispatchTakePictureIntent();
+                    Log.i("Ben", "success");
+                    Log.i("Ben", studentidContent);
+                }
+            }
+        });
+    }
+}
+
+
